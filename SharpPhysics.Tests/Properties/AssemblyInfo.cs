abfac1cb42ff3a05b0f3d37ffb024362770fc14d
Dan Kelley <dantheflipman@gmail.com>
2025-02-19 07:11:43 -0600

    Updating to .Net 8

--- SharpPhysics.Tests/Properties/AssemblyInfo.cs
+++ SharpPhysics.Tests/Properties/AssemblyInfo.cs
@@ -1,17 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("SharpPhysics.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharpPhysics.Tests")]
-[assembly: AssemblyCopyright("Copyright © 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("5b5c116e-de45-4d9c-a784-bf3b5e7c7b7a")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
