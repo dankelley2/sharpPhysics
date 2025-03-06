00b0023647ef579dbe169f66e7c4e4ec5bf91679
Dan Kelley <dantheflipman@gmail.com>
2025-03-06 12:57:14 -0600

    removing unused functions

--- physics/Engine/Helpers/SFMLExtensions.cs
+++ physics/Engine/Helpers/SFMLExtensions.cs
@@ -1,17 +1,10 @@
 using System.Numerics;
-using System.Drawing;
 using SFML.System;
 
 namespace physics.Engine.Helpers
 {
     public static class SFMLExtensions
     {
-        public static Vector2 ToVector2(this PointF point)
-            => new Vector2(point.X, point.Y);
-
-        public static PointF ToPointF(this Vector2 vector)
-            => new PointF(vector.X, vector.Y);
-
         public static Vector2f ToSfml(this Vector2 vector)
             => new Vector2f(vector.X, vector.Y);
 
